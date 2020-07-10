22d85cc99ff026bfc0e3e44a9553bc292f3c7e55
alejoceballos <alejoceballos75@gmail.com>
2020-07-09 22:18:56 -0300

    Unnecessary comments

--- app/pods/budget/service.js
+++ app/pods/budget/service.js
@@ -5,17 +5,9 @@ export default class BudgetService extends Service {
 
     async getAll() {
         return this.store.findAll('budget');
-        // const response = await fetch('/api/budgets.json');
-        // const { data } = await response.json();
-        // return data.map(model => {
-        //     const { id, attributes } = model;
-        //     return { ...attributes, id };
-        // });
     }
 
     async get(id) {
         return this.store.find('budget', id);
-        // const budgets = await this.getAll();
-        // return budgets.find(budget => budget.id == id);
     }
 }
